0a19e1513f6031942a625868f10d666431c7471d
Taruun Prasath <taruunprasathofficial@gmail.com>
2024-05-19 23:04:09 +0530

    done

---
+++ P3-Client/index.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    console.log("showMenu() called");
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    console.log("hideMenu() called");
+    navLinks.style.right = "-200px";
+}
